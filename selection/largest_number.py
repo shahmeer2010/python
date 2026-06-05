931d5f2324849b9de86e324f8a4bf6fb908ca066
shahmeer2010 <najajem4@gmail.com>
2026-06-05 18:17:20 +0500

    "changes inside the largest number file"

--- selection/largest_number.py
+++ selection/largest_number.py
@@ -1,8 +1,8 @@
 a = int(input("enter number a"))
 b = int(input("enter number b"))
-if a>b :
+if a>b:
     print("a is largest number")
-elif b>a :
+elif b>a:
     print("b is largest number")
 else :
     print("both are same")
